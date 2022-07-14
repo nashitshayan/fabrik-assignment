9267405e573f2511f61b5388d0cdd79d858aa4e2
Nashit Shayan <shayan.nashit@gmail.com>
2022-07-14 21:15:36 +0530

    Upload files

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,30 @@
+import { useRef } from 'react';
 import logo from '../images/fabrik_full_logo.png';
-function Nav() {
+function Nav({ uploadFileHandler, uploadProgress }) {
+	const hiddenFileInput = useRef(null);
+	const handleClick = (e) => {
+		hiddenFileInput.current.click();
+	};
+	const handleChange = (e) => {
+		const fileUploaded = e.target.files[0];
+		uploadFileHandler(fileUploaded);
+	};
 	return (
 		<nav>
 			<div>
 				<img src={logo} alt='logo' className='logo' />
 			</div>
 			<div>
-				<button className='btn-upload-model'>Upload 3D Model</button>
+				<button className='btn-upload-model' onClick={handleClick}>
+					Upload 3D Model
+				</button>
+				{/* {uploadProgress > 0 && <div>{uploadProgress} % uploaded</div>} */}
+				<input
+					type='file'
+					ref={hiddenFileInput}
+					onChange={handleChange}
+					style={{ display: 'none' }}
+				/>
 			</div>
 		</nav>
 	);
